06235bee410233b4102ba44d02fec1ca29c59db2
guilhermefpeixoto <guilherme.peixoto@ccc.ufcg.edu.br>
2023-03-23 23:53:55 -0300

    Adicionando alguns comandos e orientações

---
+++ src/exercicios/utilidade.clj
@@ -0,0 +1,20 @@
+(ns exercicios.utilidade)
+
+; Para utilizar o REPl em um arquivo específico,
+; primeiro se roda o REPL no project.clj e depois
+; entra no arquivo escolhido e utiliza alt shift r
+; e em seguida alt shift l. Se eu criar uma nova
+; função, é necessário recarregar (alt shift l) para
+; utilizá-la.
+
+; Para selecionar um elemento de um vetor, se utiliza
+; (vetor x), em que x é o índice do elemento. Além disso,
+; pode-se utilizar o comando (get vetor índice default[opcional]). Dar
+; preferência ao get.
+
+; Alt shift k "engole" o trecho de código que vem em seguida,
+; pode ser útil pra colocar parâmetros em uma função, por
+; exemplo. Aumenta o escopo.
+
+; Para alterar um elemento de uma posição específica de um vetor
+; pode-se utilizar (assoc vetor índice novo-elemento).
